aac7d7284257ff582ef65b2a0b47b3a215fd65ef
biscas <ricardopintokk@hotmail.com>
2014-03-12 15:17:05 +0000

    f3_ex2.py

---
+++ f3_ex2.py
@@ -0,0 +1,22 @@
+import pyafai
+from pyafai import objects
+from pyafai import shapes
+from random import randint
+
+def main():
+	world = pyafai.World2D()
+	display =  pyafai.Display(world)
+
+	obj = objects.pingPong(200,200)
+
+	shape = shapes.Circle(10)
+	obj.add_shape(shape)
+	obj.angle = randint(10,360) 
+	obj.velocity = 300#randint(50,350)
+	 
+	world.add_object(obj)
+
+	pyafai.run()
+
+if __name__ == '__main__':
+	main()
